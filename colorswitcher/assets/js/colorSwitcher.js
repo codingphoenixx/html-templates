249a20286e738b9a64f1e4e4b78dab1a6713389d
CodingPhoenixx <72647759+codingphoenixx@users.noreply.github.com>
2025-04-01 10:07:05 +0200

    Added IFrame Support for color switcher

--- colorswitcher/assets/js/colorSwitcher.js
+++ colorswitcher/assets/js/colorSwitcher.js
@@ -15,12 +15,38 @@ function loadTheme() {
 
 function setTheme(theme) {
     updateLocalStorage(theme)
+
     document.documentElement.setAttribute("data-theme", theme);
+
+    document.querySelectorAll("iframe").forEach((iframe) => {
+        try {
+            if (iframe.contentDocument) {
+                iframe.contentDocument.documentElement.setAttribute("data-theme", theme);
+            }
+        } catch (e) {
+            console.warn("Konnte Theme nicht auf iFrame setzen:", e);
+        }
+    });
+
     console.info("Set theme to " + theme)
 }
 
 async function updateLocalStorage(theme) {
     localStorage.setItem("theme", theme);
 }
+
+function updateIFrames(){
+    document.querySelectorAll("iframe").forEach((iframe) => {
+        iframe.addEventListener("load", function () {
+            try {
+                const currentTheme = document.documentElement.getAttribute("data-theme");
+                this.contentDocument.documentElement.setAttribute("data-theme", currentTheme);
+            } catch (e) {
+                console.warn("Konnte Theme nicht auf geladenen iFrame setzen:", e);
+            }
+        });
+    });
+}
+
 
 loadTheme();
